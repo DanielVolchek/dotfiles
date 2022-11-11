8c291dce3a3ae40c6263c576f511794203272092
Daniel Volchek <danielvolchek123@gmail.com>
2022-11-11 17:16:43 -0500

    modified zsh

--- zsh/beforeprompt.zsh
+++ zsh/beforeprompt.zsh
@@ -1,4 +1,3 @@
 # everything that needs to happen before instantprompt is loading
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.  
- 
