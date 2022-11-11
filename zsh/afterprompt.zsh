c9633debd53fc81fd594e02c423d3f12ced6f4dd
Daniel Volchek <danielvolchek123@gmail.com>
2022-11-11 16:57:10 -0500

    further modified zsh structure

---
+++ zsh/afterprompt.zsh
@@ -0,0 +1,7 @@
+# just for fun
+neofetch --gap 10
+ 
+# start lightweight spotify client for use with SPT (if env is true)
+START_SPOT=true
+$START_SPOT && spotifyd
+
--- zsh/beforeprompt.zsh
+++ zsh/beforeprompt.zsh
@@ -1,8 +1,4 @@
-# everything that needs to happen while instantprompt is loading
+# everything that needs to happen before instantprompt is loading
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.  
  
-# just for fun
-neofetch --gap 10
- 
-# start lightweight spotify client for use with SPT (if env is true)
-START_SPOT=true
-$START_SPOT && spotifyd
