3397ad78969ac175e6ebc5328f4b9973f70b0adf
Daniel Volchek <danielvolchek123@gmail.com>
2022-11-12 01:43:44 -0500

    init changed

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,23 +1,14 @@
 -- setup
 require("daniel.plugins-setup")
+
+-- load config
 require("daniel.config")
 
 --load core
-require("daniel.core.options") -- opts
-require("daniel.core.keymaps") -- keymaps
-require("daniel.core.colorscheme") -- colorscheme
-require("daniel.core.commands") -- self-defined commands
+require("daniel.core")
 
 -- load plugins
 config.sourcedir(vim.fn.stdpath("config") .. "/lua/daniel/plugins")
 
 --load maps
-for mode, maps in pairs(config.maps) do
-	for key, map in pairs(maps) do
-		local opts = map.opts or {}
-		opts.desc = opts.desc or maps.desc or "No Description Provided"
-		opts.noremap = true
-		opts.silent = true
-		vim.api.nvim_set_keymap(mode, key, map[0], opts)
-	end
-end
+-- config.loadmaps(config.maps)
