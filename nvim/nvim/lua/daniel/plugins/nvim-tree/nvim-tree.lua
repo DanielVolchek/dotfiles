5724eba0f7c118c2b98ed8baa914bb0bf8ddaba1
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-01 08:49:46 -0800

    fixed breaking change in keymaps

--- nvim/nvim/lua/daniel/plugins/nvim-tree/nvim-tree.lua
+++ nvim/nvim/lua/daniel/plugins/nvim-tree/nvim-tree.lua
@@ -72,7 +72,7 @@ local opts = {
 		mappings = {
 			custom_only = false,
 			list = {
-				{ key = "l", action = "preview" },
+				{ key = "l", action = "edit", action_cb = edit_or_open },
 				{ key = "L", action = "edit", action_cb = edit_or_open },
 				{ key = "h", action = "close_node" },
 				{ key = "H", action = "collapse_all", action_cb = collapse_all },
