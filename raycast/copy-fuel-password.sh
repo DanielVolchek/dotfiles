c1a04dbfceb4fefcfa31d1717cd3d18f8e089ee6
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-07 13:07:18 -0500

    added raycast command script

---
+++ raycast/copy-fuel-password.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Copy Fuel Password
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+cat PASS.txt | pbcopy
+
