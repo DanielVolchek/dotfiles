f39ad462bf48d515ee139d51f5ff377674dde997
Daniel Volchek <danielvolchek@Daniels-MacBook-Pro.local>
2023-04-04 14:11:49 -0400

    add check for work env and home env
    add check

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,13 +1,33 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+# silicon binds
+if [[ $ENV == "HOME" ]]; then
+  if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+    PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  # silicon bindings
+  source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+  #intel bindings
+else
+# intel binds
+  # Setup fzf
+# ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
