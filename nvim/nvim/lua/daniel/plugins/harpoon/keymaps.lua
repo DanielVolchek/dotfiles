34c883d0eb77ec8691bec63106bb54b7c019f197
Daniel Volchek <danielvolchek@Daniels-MacBook-Pro.local>
2023-05-24 18:28:11 -0400

    modified harpoon keymaps to be more consistent

--- nvim/nvim/lua/daniel/plugins/harpoon/keymaps.lua
+++ nvim/nvim/lua/daniel/plugins/harpoon/keymaps.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Harpoon Add" })
-vim.keymap.set("n", "<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Harpoon Marks" })
+vim.keymap.set("n", "a", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Harpoon Add" })
+vim.keymap.set("n", "m", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Harpoon Marks" })
 
 vim.keymap.set("n", "h", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
 vim.keymap.set("n", "j", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
