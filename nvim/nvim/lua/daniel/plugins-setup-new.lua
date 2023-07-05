ef07eddc928e306475c0d619dd4c3c738331c4a7
Daniel Volchek <danielvolchek123@gmail.com>
2023-07-05 10:17:25 -0400

    plsetupnew

--- nvim/nvim/lua/daniel/plugins-setup-new.lua
+++ nvim/nvim/lua/daniel/plugins-setup-new.lua
@@ -95,24 +95,24 @@ require("lazy").setup({
 			require("nvim-treesitter.install").update({ with_sync = true })
 		end,
 	},
-	{
-		"zbirenbaum/copilot.lua",
-		dependencies = "lualine.nvim", -- whichever statusline plugin you use here
-		config = function()
-			vim.defer_fn(function()
-				require("copilot").setup({})
-			end, 100)
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		dependencies = { "copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup({
-				formatters = {
-					insert_text = require("copilot_cmp.format").remove_existing,
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"zbirenbaum/copilot.lua",
+	-- 	dependencies = "lualine.nvim", -- whichever statusline plugin you use here
+	-- 	config = function()
+	-- 		vim.defer_fn(function()
+	-- 			require("copilot").setup({})
+	-- 		end, 100)
+	-- 	end,
+	-- },
+	-- {
+	-- 	"zbirenbaum/copilot-cmp",
+	-- 	dependencies = { "copilot.lua" },
+	-- 	config = function()
+	-- 		require("copilot_cmp").setup({
+	-- 			formatters = {
+	-- 				insert_text = require("copilot_cmp.format").remove_existing,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 })
