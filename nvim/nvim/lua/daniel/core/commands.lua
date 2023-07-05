afa590fb61d882d636c26f3081a5d22efd095c63
Daniel Volchek <daniel.volchek@clover.com>
2023-07-05 09:14:22 -0400

    add FullPath command

--- nvim/nvim/lua/daniel/core/commands.lua
+++ nvim/nvim/lua/daniel/core/commands.lua
@@ -9,6 +9,7 @@ vim.cmd("command EditConf e ~/.config/nvim/init.lua")
 vim.cmd("command Dash Dashboard")
 vim.cmd("command CD lcd %:p:h")
 vim.cmd("command Diff :w !diff % -")
+vim.cmd("command FullPath :!echo % | pbcopy")
 
 vim.cmd("command SplitCount :lua P(#vim.api.nvim_tabpage_list_wins(0))")
 -- vim.cmd("command CDCurrentGlobal lcd %:p:h")
