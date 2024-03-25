28f5201fdb50d4d022fff5dba4332839e6cd4e57
Daniel Volchek <daniel.volchek@clover.com>
2024-03-25 09:19:04 -0400

    har filetype

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -26,6 +26,15 @@ config.sourcedir(vim.fn.stdpath("config") .. "/lua/daniel/plugins")
 
 -- config.loadmaps(config.maps)
 
+-- Set filetype for har
+-- TODO create new file in .core lib for autcmds
+vim.api.nvim_exec(
+	[[
+  autocmd BufNewFile,BufRead *.har set filetype=har
+]],
+	false
+)
+
 -- disable comment continuation
 vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
